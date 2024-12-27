fec460ae04470162ef8bd298907a3a7fb257049b
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2024-12-27 14:08:58 -0300

    Project initialized

---
+++ src/test/java/br/edu/ifpb/usersManagement/UsersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.usersManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
